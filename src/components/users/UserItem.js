022827c9fdb5ae1b2c72570e0dc5c4476add044e
Thomas Martinez <tmartinez114@gmail.com>
2020-12-07 00:02:26 -0500

    refactored

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 
 export class UserItem extends Component {
-    constructor() {
-        super();
-        this.state = {
-          id: 'id',
-          login: 'thomasmartinez114',
-          avatar_url: 'https://avatars0.githubusercontent.com/u/48811054?s=460&u=e32dee2196108dba9bde7096a39e571b6e7bf19e&v=4',
-          html_url: 'https://github.com/thomasmartinez114'
-        }
+    state = {
+        id: 'id',
+        login: 'thomasmartinez114',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/48811054?s=460&u=e32dee2196108dba9bde7096a39e571b6e7bf19e&v=4',
+        html_url: 'https://github.com/thomasmartinez114'
       }
 
     render() {
