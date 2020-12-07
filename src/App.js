cd4cf6cb8058bd6a96827eddc12ef153e6a3ad24
Thomas Martinez <tmartinez114@gmail.com>
2020-12-06 23:13:28 -0500

    Using Tenary conditionals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className="App">
+      {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
