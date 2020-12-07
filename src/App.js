dc33b0d62628e253108dfdabad11d24e217c90c3
Thomas Martinez <tmartinez114@gmail.com>
2020-12-06 23:28:34 -0500

    set up Default Props if nothing was passed in as a prop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar title="GitHub Finder" icon="fab fa-github"/>
+      <Navbar/>
       </div>
     );
   }
