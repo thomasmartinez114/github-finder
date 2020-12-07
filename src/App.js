f65ed1499596deb400822250ba789b4fddfa2596
Thomas Martinez <tmartinez114@gmail.com>
2020-12-06 23:27:26 -0500

    set up props to pass title and icon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar/>
+      <Navbar title="GitHub Finder" icon="fab fa-github"/>
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Navbar</h1>
-            </div>
+            <nav className="navbar bg-primary">
+                <h1>
+                <i className={this.props.icon} /> {this.props.title}
+                </h1>
+            </nav>
         )
     }
 }
