340118b09ed8c4a9abcb06f1de8884b3438aecb0
Thomas Martinez <tmartinez114@gmail.com>
2020-12-07 23:43:20 -0500

    Users.js -- loop through array of users state and display on App.js

---
+++ src/components/users/Users.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export class Users extends Component {
+    state = {
+        users: [
+            {
+                id: '1',
+                login: 'thomasmartinez114',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                html_url: 'https://github.com/thomasmartinez114'
+            },
+            {
+                id: '2',
+                login: 'mojombo',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/48811054?s=460&u=e32dee2196108dba9bde7096a39e571b6e7bf19e&v=4',
+                html_url: 'https://github.com/mojombo'
+            },
+            {
+                id: '3',
+                login: 'defunkt',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+                html_url: 'https://github.com/defunkt'
+            }
+        ]
+    }
+    render() {
+        return (
+            <div>
+                {this.state.users.map(user => (
+                    <div>{user.login}</div>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Users
