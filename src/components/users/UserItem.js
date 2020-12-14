da8dedf63a03868258d23dcd5c53363f7f1c9d79
Thomas Martinez <tmartinez114@gmail.com>
2020-12-13 23:34:43 -0500

    userIiem more refactor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-const UserItem = (props) => {
-        const { login, avatar_url, html_url } = props.user;
+const UserItem = ({user: { login, avatar_url, html_url }}) => {
 
-        return <div className="card text-center">
-                <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }}
-                />
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">GitHub Page</a>
-                </div>
+return (
+<div className="card text-center">
+        <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+        <div>
+            <a href={html_url} className="btn btn-dark btn-sm my-1">GitHub Page</a>
+        </div>
 
-            </div>;
+    </div>
+    )
 }
 
 export default UserItem;
