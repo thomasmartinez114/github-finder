63b3cf5be1da049a1852a548320981907a872625
Thomas Martinez <tmartinez114@gmail.com>
2020-12-13 22:59:41 -0500

    displaying the users in the state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react'
 
 export class UserItem extends Component {
-    state = {
-        id: 'id',
-        login: 'thomasmartinez114',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/48811054?s=460&u=e32dee2196108dba9bde7096a39e571b6e7bf19e&v=4',
-        html_url: 'https://github.com/thomasmartinez114'
-      }
-
     render() {
-
-        const { login, avatar_url, html_url } = this.state;
+        const { login, avatar_url, html_url } = this.props.user;
 
         return <div className="card text-center">
                 <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }}
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import UserItem from './UserItem';
 
 export class Users extends Component {
     state = {
@@ -27,7 +28,7 @@ export class Users extends Component {
         return (
             <div>
                 {this.state.users.map(user => (
-                    <div key={user.id}>{user.login}</div>
+                    <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
